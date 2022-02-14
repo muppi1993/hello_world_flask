0bff7448ba1b07a7da14255e969bb9ad65eb61b4
muppi1993 <clara.gronemeyer20@imperial.ac.uk>
2022-02-14 15:57:14 +0000

    included json schema

--- app.py
+++ app.py
@@ -1,29 +1,30 @@
 from flask import Flask
-from flask import url_for
 from flask import request
 from flask import jsonify
+from flask_expects_json import expects_json
+import datetime
 
 app = Flask(__name__)
+
+schema = {
+  "type": "object",
+  "properties": {
+    "firstname": { "type": "string" },
+    "lastname": { "type": "string" },
+    "age": {"type": "number"}
+  },
+  "required": ["firstname"]
+}
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
-
-@app.route("/login")
-def login_page():
-    # Process login
-    return 'Hello, User!'
-
-@app.route("/login2/<user>")
-def login_user(user):
-    # User login flow.
-    return user
-
-@app.route("/print")
-def redirect_to_login():
-    return url_for('login_user',user='some_user_name')
-
-
+    
 @app.route("/json", methods=['POST'])
+@expects_json(schema)
 def read_json():
-    return jsonify({"greeting": "Hello, " + request.json['name']})
+    timestamp = datetime.datetime.now().isoformat()
+    if 'lastname' in request.json:
+        return jsonify({"greeting": "Hello, " + request.json['firstname']+" "+request.json['lastname'], "timestamp":timestamp})
+    else:
+        return jsonify({"greeting": "Hello, " + request.json['firstname'], "timestamp":timestamp})
