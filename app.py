30f4ccdee5157f21def8c10b8052914d3045d6f8
muppi1993 <clara.gronemeyer20@imperial.ac.uk>
2022-02-15 15:28:18 +0000

    Added rq to run both simple functions and poppunk

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from flask import jsonify
 from flask_expects_json import expects_json
 import datetime
 
+from rq_function import add, count_lines, poppunk_assign
+from redis import Redis
+from rq import Queue
+
 app = Flask(__name__)
 
 schema = {
@@ -16,10 +20,20 @@ schema = {
   "required": ["firstname"]
 }
 
+schema2 = {
+  "type": "object",
+  "properties": {
+    "path": { "type": "string" }
+  },
+  "required": ["path"]
+}
+
+#homepage
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
     
+#take json input (according to schema) and return json
 @app.route("/json", methods=['POST'])
 @expects_json(schema)
 def read_json():
@@ -28,3 +42,22 @@ def read_json():
         return jsonify({"greeting": "Hello, " + request.json['firstname']+" "+request.json['lastname'], "timestamp":timestamp})
     else:
         return jsonify({"greeting": "Hello, " + request.json['firstname'], "timestamp":timestamp})
+    #curl -H "Content-Type: application/json" --data '{"firstname": "Marie","lastname":"Gronemeyer","age":29}' http://127.0.0.1:5000/json
+
+#run some jobs in a rq
+@app.route("/rq")
+def try_rq():
+  q = Queue(connection=Redis())
+  result = q.enqueue(add, 5,5)
+  result2 = q.enqueue(count_lines, 'poppunk/input_data/qfile.txt')
+  return "All jobs submitted\n"
+  #curl http://127.0.0.1:5000/rq
+
+#run poppunk in a rq
+@app.route("/poppunk", methods=['POST'])
+@expects_json(schema2)
+def try_poppunk():
+  q = Queue(connection=Redis())
+  result = q.enqueue(poppunk_assign,request.json['path'])
+  return "All jobs submitted\n"
+  #curl -H "Content-Type: application/json" --data '{"path":"/home/mmg220/Documents/poppunk"}' http://127.0.0.1:5000/poppunk
---
+++ rq_function.py
@@ -0,0 +1,21 @@
+import requests
+import os
+
+def add(a,b):
+    print(a+b)
+
+def count_lines(path):
+    file = open(path, "r")
+    line_count = 0
+    for line in file:
+        if line != "\n":
+            line_count += 1
+    file.close()
+    print(line_count)
+
+def poppunk_assign(path):
+    os.system('''
+    cd %s
+    poppunk_assign --db GPS_v4 --query input_data/qfile.txt --output poppunk_clusters_flask --threads 4
+    '''
+    %path)
