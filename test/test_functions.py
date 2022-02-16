9ac68caf93138af7d7c2ae31def26d03f0a1fead
muppi1993 <clara.gronemeyer20@imperial.ac.uk>
2022-02-16 17:11:27 +0000

    included simple pytests

---
+++ test/test_functions.py
@@ -0,0 +1,14 @@
+import rq_function
+
+def test_add():
+    assert rq_function.add(5,4) == 9
+    assert rq_function.add(5,10) == 15
+    assert rq_function.add(0,0) == 0
+
+def test_count_lines():
+    assert rq_function.count_lines('test/testfile1.txt') == 14
+    assert rq_function.count_lines('test/testfile2.txt') == 11
+
+def test_poppunk():
+    #not really helpful yet, needs improvement, e.g. checking if files got saved?
+    assert rq_function.poppunk_assign("/home/mmg220/Documents/flask-tutorial/poppunk") == "/home/mmg220/Documents/flask-tutorial/poppunk/poppunk_clusters_flask"
